16ba5e4175c9a8de3b141c779c86f24b1b054380
Spencer Machado <admiralmachado@gmail.com>
2013-03-02 14:39:12 -0700

    Made Room, Wall, Door, Camera, Sidekick Prefabs
    Added rooms (room, door, walls), a sidekick prefab with keyboard
    movement controls and a camera prefab that can be used to replace the
    "Main Camera" object in a new scene.
    Tutorial on how to use it here: http://youtu.be/AlcKekFd1ls

---
+++ Assets/RoomController.js
@@ -0,0 +1,16 @@
+#pragma strict
+
+var RoomToNorth : GameObject;
+var RoomToSouth : GameObject;
+var RoomToEast : GameObject;
+var RoomToWest : GameObject;
+
+function Start () {
+	if (RoomToNorth != null) {
+		
+	}
+}
+
+function Update () {
+
+}
